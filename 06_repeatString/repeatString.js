f84331d2e2f68175367b08e341d1fe375324c90b
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-10-01 22:31:10 +0530

    Completed exercise 6 and 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word,number) {
+    let word1='';
+for(let i=0;i<number;i++){
+word1+=word;
+}
+return word1;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let arr=word.split('');
+arr.reverse();
+word=arr.join('');
+return word;
 };
 
 // Do not edit below this line
