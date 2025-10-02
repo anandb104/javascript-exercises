00d3d94dc7d62045f1d53329a847ca364ef260d0
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-10-02 19:25:53 +0530

    Complete all tests of exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let y= (((num-32)*5)/9);
+  let z=Math.round(y * 10) / 10
+  let p=+z;
+  return p;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let y= ((num*(9/5))+32)
+  let z=Math.round(y * 10) / 10
+  let p=Number(z);
+  return p;
 };
 
 // Do not edit below this line
