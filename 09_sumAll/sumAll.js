d5da08af384181b26706ef18f2bff78bdc6f40b5
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-10-02 18:58:38 +0530

    Complete all test of exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum=0;
+    if(num1<0 || num2<0) return "ERROR";
+    if(typeof num1!="number" || typeof num2!="number") return "ERROR";
+    if(Number.isInteger(num1)!=true || Number.isInteger(num2)!=true) return "ERROR";
+   if(num2>num1){
+     for(let i=num1;i<=num2;i++){
+        sum+=i;
+     }
+   }
+   else{
+    for(let i=num2;i<=num1;i++){
+        sum+=i;
+     }
+   }
+   return sum;
 };
 
 // Do not edit below this line
