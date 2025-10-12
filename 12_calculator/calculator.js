e1d3a2a6088c6bca5f47e445ff5f37ce1255ec31
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-10-12 13:54:58 +0530

    Complete all tests of exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum=0;
+  for(let key in arr){
+    sum+=arr[key];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product=1;
+  for(let key in arr){
+    product*=arr[key];
+  }
+return product;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let factori=1;
+  for(let i=num1;i>0;i--){
+    factori*=i;
+  }
+  return factori;
 };
 
 // Do not edit below this line
