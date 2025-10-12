befee05eb0b8434e7f345c8ec7e67c96468e9569
anandb104 <210169445+anandb104@users.noreply.github.com>
2025-10-12 23:14:58 +0530

    Complete all tests of exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let currentDate = new Date();
+arr.sort((a,b)=>{if(a.yearOfDeath==undefined){
+    a.yearOfDeath=currentDate.getFullYear()
+}
+if(b.yearOfDeath==undefined){
+    b.yearOfDeath=currentDate.getFullYear()
+}
+   return (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth)});
 
+return arr[0];
 };
 
 // Do not edit below this line
